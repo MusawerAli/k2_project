fcea4ad224984f713afc2845b9efcac7f13190b9
MusawerAli <pakjalihouse@gmail.com>
2019-02-04 23:41:22 -0800

    some new update in (index.php)

--- index.php
+++ index.php
@@ -1,113 +1,103 @@
-<!-- <?php
- ///include "http://localhost/k2_project/html/header_footer/header.php";
- require_once('html/header_footer/header.php');
-?> -->
+<?php
+///include "http://localhost/k2_project/html/header_footer/header.php";
+require_once('html/header_footer/header.php');
+?>
 <style>
 .featured-title{
-  overflow:hidden;
+overflow:hidden;
 }
 .featured-title:after{
-  content:'';
-  display:inline-block;
-  width:80%; height:100%;
-  margin-right:-100%;
-  border-bottom:6px solid white;
+content:'';
+display:inline-block;
+width:80%; height:100%;
+margin-right:-100%;
+border-bottom:6px solid white;
 }
-
 </style>
 <!-- Our Services -->
-  <!-- Header -->
-<header class="w3-display-container w3-content w3-center" style="max-width:1500px">
+<!-- Header -->
+<header class="w3-display-container w3-content w3-center" style="max-width:1500px;margin-top: 140px">
   <img class="w3-image" src="assets/image/Website-design-1.png" alt="Me" width="2000" height="400">
   <div class="w3-display-right w3-padding-large w3-border w3-wide w3-text-black w3-white w3-grayscale-max w3-opacity-min w3-center">
-      <i><img src="http://localhost/k2_project/assets/image/m.png" height="40px" alt="Italian Trulli"></i>
-    <h1 class="w3-hide-medium w3-hide-small w3-xxxlarge"></h1>
+    <i><img src="http://localhost/k2_project/assets/image/logo1.png" height="40px" alt="Italian Trulli"></i>
+    <i><img src="http://localhost/k2_project/assets/image/logo2.png" height="40px" alt="Italian Trulli"></i>
+    <p class="">LUXURY PROPERT<br>IN CENTRAL LONDON</p>
     <h5 class="w3-hide-large" style="white-space:nowrap">JANE DOE</h5>
     <h3 class="w3-hide-medium w3-hide-small">PHOTOGRAPHER</h3>
   </div>
 </header>
-
-  <div class="w3-row-padding w3-grayscale">
-    <div class="w3-col  m4 w3-margin-bottom">
-      <img src="assets/image/Website-design-2.png" alt="John" style="width:100%">
-      <h3>Property Managment</h3>
-      <p class="w3-opacity">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-      <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
-     
-    </div>
-    <div class="w3-col  m4 w3-margin-bottom">
-      <img src="assets/image/Website-design-3.png" alt="Jane" style="width:100%">
-      <h3>Property Sales</h3>
-      <p class="w3-opacity">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-      <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
-     
-    </div>
-    <div class="w3-col  m4 w3-margin-bottom">
-      <img src="assets/image/Website-design-4.png" alt="Mike" style="width:100%">
-      <h3>Property Littings</h3>
-      <p class="w3-opacity">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-      <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
-   
-    </div>
+<div class="w3-row-padding w3-grayscale">
+  <div class="w3-col  m4 w3-margin-bottom">
+    <img src="assets/image/Website-design-2.png" alt="John" style="width:100%">
+    <h3>Property Managment</h3>
+    <p class="w3-opacity">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+    <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
     
   </div>
+  <div class="w3-col  m4 w3-margin-bottom">
+    <img src="assets/image/Website-design-3.png" alt="Jane" style="width:100%">
+    <h3>Property Sales</h3>
+    <p class="w3-opacity">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+    <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
     
+  </div>
+  <div class="w3-col  m4 w3-margin-bottom">
+    <img src="assets/image/Website-design-4.png" alt="Mike" style="width:100%">
+    <h3>Property Littings</h3>
+    <p class="w3-opacity">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+    <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+    
+  </div>
+  
+</div>
 <!-- Features Property -->
-
-
 <div class="w3-row-padding w3-grayscale" style="background-color: rgb(133, 116, 102);">
-  <h1 class="featured-title">Featured POst</h1>
-
-    <div class="w3-col  m6 w3-margin-bottom">
-      <img src="assets/image/Website-design-5.png" alt="John" style="width:100%;">
-      <h3>Church Street Sw2</h3>
-      <p class="w3-opacity">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-      <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
-     
-    </div>
-    <div class="w3-col  m6 w3-margin-bottom">
-      <img src="assets/image/Website-design-6.png" alt="Jane" style="width:100%">
-      <h3>Luxury Development sw1</h3>
-      <p class="w3-opacity">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-      <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
-     
-    </div>
-   
+  <h1 class="featured-title w3-wide">FEATURED POST</h1>
+  <div class="w3-col  m6 w3-margin-bottom">
+    <img src="assets/image/Website-design-5.png" alt="John" style="width:100%;">
+    <h3>Church Street Sw2</h3>
+    <p class="w3-opacity">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+    <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
     
   </div>
+  <div class="w3-col  m6 w3-margin-bottom">
+    <img src="assets/image/Website-design-6.png" alt="Jane" style="width:100%">
+    <h3>Luxury Development sw1</h3>
+    <p class="w3-opacity">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+    <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+    
+  </div>
+  
+  
+</div>
+<hr>
 <!-- News Section -->
 <div class="w3-row-padding w3-grayscale" style="background-color: rgb(82, 71, 63);">
-   <h1 class="featured-title">Latest News </h1>
-    <div class="w3-col  m4 w3-margin-bottom">
-      
-      <h3>Property Managment</h3>
-      <p class="w3-opacity">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-      <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
-      <button class="w3-btn w3-gray">Find Out More</button>
-     
-    </div>
-    <div class="w3-col  m4 w3-margin-bottom">
-     
-      <h3>Property Sales</h3>
-      <p class="w3-opacity">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-      <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
-      <button class="w3-btn w3-gray">Find Out More</button>
-    </div>
-    <div class="w3-col  m4 w3-margin-bottom">
-      
-      <h3>Property Littings</h3>
-      <p class="w3-opacity">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-      <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
-    <button class="w3-btn w3-gray">Find Out More</button>
-    </div>
-    </div>
+  <h1 class="featured-title w3-wide">LATEST NEWS </h1>
+  <div class="w3-col  m4 w3-margin-bottom" style="background-color: rgb(133, 116, 102);">
     
-
-
-
-
-
+    <h3>Property Managment</h3>
+    <p class="w3-opacity">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+    <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+    <button class="w3-btn w3-gray">Find Out More</button>
+    
+  </div>
+  <div class="w3-col  m4 w3-margin-bottom" style="background-color: rgb(243, 131, 102);">
+    
+    <h3>Property Sales</h3>
+    <p class="w3-opacity">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+    <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+    <button class="w3-btn w3-gray">Find Out More</button>
+  </div>
+  <div class="w3-col  m4 w3-margin-bottom" style="background-color: rgb(133, 116, 102);">
+    
+    <h3>Property Littings</h3>
+    <p class="w3-opacity">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+    <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+    <button class="w3-btn w3-gray">Find Out More</button>
+  </div>
+</div>
 <?php
- //include "http://localhost/k2_project/html/header_footer/header.php";
- require_once('html/header_footer/footer.php');
+//include "http://localhost/k2_project/html/header_footer/header.php";
+require_once('html/header_footer/footer.php');
 ?>
