22354ccf04ef4c1acf9d59d245f2b4c9bacea2b7
MusawerAli <pakjalihouse@gmail.com>
2019-02-04 23:40:25 -0800

    property Managment(Complete)

---
+++ html/web/property_managment.php
@@ -0,0 +1,73 @@
+<?php
+ ///include "http://localhost/k2_project/html/header_footer/header.php";
+ require_once('../header_footer/header.php');
+?> 
+
+
+
+<!-- Header -->
+<header class="w3-display-container w3-content w3-wide" style="max-width:1600px;min-width:500px;margin-top:200px;"  width="1600" height="600" id="home">
+  <img class="w3-image" src="../../assets/image/Website-design-10.png"  alt="Hamburger Catering">
+  <div class="w3-display-middle w3-center w3-text-black w3-white w3-grayscale-max w3-opacity-min">
+    <p class="w3-jumbo">Just Lets</p>
+    <p class="w3-hide">Just Lets</p>
+   
+  <p class="w3-large w3-text-black w3-hide-small w3-text-white ">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit,</p>
+  <button class="w3-btn w3-black">More Info >></button>
+  </div>
+</header>
+  <!-- About Section -->
+  <div class="w3-row w3-padding-64" id="landlords" style="background-color: rgb(133, 116, 102);">
+    <div class="w3-col m6 w3-padding-large ">
+     <img src="../../assets/image/Website-design-2.png" class="w3-round w3-image w3-opacity-min" alt="Table Setting" >
+    </div>
+
+    <div class="w3-col m6 w3-padding-large">
+      <h1 class="w3-center">Land Loards</h1><br>
+      <h5 class="w3-center">Tradition since 1889</h5>
+      <p class="w3-large">The Catering was founded in blabla by Mr. Smith in lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute iruredolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.We only use <span class="w3-tag w3-light-grey">seasonal</span> ingredients.</p>
+      <p class="w3-large w3-text-white ">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+  </div>
+  
+  <hr>
+  
+
+<!-- Lettings -->
+<!-- Menu Section -->
+  <div class="w3-row w3-padding-64" id="lettings" style="background-color: rgb(133, 116, 102);">
+    <div class="w3-col l6 w3-padding-large">
+         <h1 class="w3-center">Lettings</h1><br>
+      <h5 class="w3-center">Tradition since 1889</h5>
+      <p class="w3-large">The Catering was founded in blabla by Mr. Smith in lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute iruredolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.We only use <span class="w3-tag w3-light-grey">seasonal</span> ingredients.</p>
+      <p class="w3-large w3-text-white ">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+    
+    <div class="w3-col l6 w3-padding-large">
+      <img src="../../assets/image/Website-design-12.png" class="w3-round w3-image w3-opacity-min" alt="Menu" style="width:100%">
+    </div>
+  </div>
+
+  <hr>
+
+ <!-- managment -->
+  <div class="w3-row w3-padding-64" id="managment" style="background-color: rgb(133, 116, 102);">
+    <div class="w3-col m6 w3-padding-large ">
+     <img src="../../assets/image/Website-design-13.png" class="w3-round w3-image w3-opacity-min" alt="Table Setting" >
+    </div>
+
+    <div class="w3-col m6 w3-padding-large">
+      <h1 class="w3-center">Managements</h1><br>
+      <h5 class="w3-center">Tradition since 1889</h5>
+      <p class="w3-large">The Catering was founded in blabla by Mr. Smith in lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute iruredolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.We only use <span class="w3-tag w3-light-grey">seasonal</span> ingredients.</p>
+      <p class="w3-large w3-text-white ">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+  </div>
+  
+  <hr>
+
+
+<?php
+ //include "http://localhost/k2_project/html/header_footer/header.php";
+ require_once('../header_footer/footer.php');
+?>
