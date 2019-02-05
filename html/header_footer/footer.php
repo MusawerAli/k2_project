58f45b8fe7810a773f5c86ae5cff5911b82fc8de
MusawerAli <pakjalihouse@gmail.com>
2019-02-04 23:42:07 -0800

    add new pic and updates in header/footer

--- html/header_footer/footer.php
+++ html/header_footer/footer.php
@@ -1,5 +1,4 @@
 <!-- Footer -->
-
 <hr style="height:3px;border:none;color:#333;background-color:#333;">
 <footer class="w3-padding-64 w3-light-grey w3-small w3-center" id="footer">
 	<div class="col-sm-8">
@@ -47,8 +46,33 @@
 	</div>
 	
 </footer>
-
-
-
+<script>
+	// Modal Image Gallery
+function onClick(element) {
+document.getElementById("img01").src = element.src;
+document.getElementById("modal01").style.display = "block";
+var captionText = document.getElementById("caption");
+captionText.innerHTML = element.alt;
+}
+// Change style of navbar on scroll
+window.onscroll = function() {myFunction()};
+function myFunction() {
+var navbar = document.getElementById("myNavbar");
+if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+} else {
+navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+}
+}
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+var x = document.getElementById("navDemo");
+if (x.className.indexOf("w3-show") == -1) {
+x.className += " w3-show";
+} else {
+x.className = x.className.replace(" w3-show", "");
+}
+}
+</script>
 </body>
 </html>
--- html/header_footer/header.php
+++ html/header_footer/header.php
@@ -1,56 +1,61 @@
-
-        <!-- CSS -->
-        <!DOCTYPE html>
-        <html>
-        <head>
-          <meta charset="utf-8">
-          <meta http-equiv="X-UA-Compatible" content="IE=edge">
-          <meta name="viewport" content="width=device-width, initial-scale=1">
-          <title></title>
-          
-          <link href="http://localhost/k2_project/assets/css/bootstrap.min.css" rel="stylesheet">
-<link href="http://localhost/k2_project/assets/css/w3.css" rel="stylesheet">
-<link href="http://localhost/k2_project/assets/css/font-awesome.min.css" rel="stylesheet">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-   
-
-       <!-- JS -->
-<script src="http://localhost/k2_project/assets/js/popper.min.js"></script>
-<script src="http://localhost/k2_project/assets/js/bootstrap.min.js"></script>
-<script src="http://localhost/k2_project/assets/js/jquery.min.js"></script>
-
-        </head>
-        <body>
-          
-    <!-- Navbar -->
-<div class="w3-top">
-  <div class="w3-bar  w3-card w3-text-white" style="background-color: rgb(64, 47, 35);"><p style="margin-left: 50px; letter-spacing: 3px;font-family: 'Times New Roman'; font-size: 20px">Email INFO@ENNISOMORE ESTATE  |  Call: +44(0)20 3123 5677</p>
-  </div>
-  <div class="w3-bar w3-card w3-padding">
-    <i><img src="http://localhost/k2_project/assets/image/m.png" height="40px" alt="Italian Trulli"></i>
-     <i class="fa fa-instagram fa-2x w3-right w3-hover-opacity"></i>
-  <i class="fa fa-twitter fa-2x w3-right w3-hover-opacity"></i>
-  <i class="fa fa-linkedin fa-2x w3-right w3-hover-opacity"></i>
-  </div>
-  <div class="w3-bar w3-card ">
-     <i><img src="http://localhost/k2_project/assets/image/google.png" height="40px" alt="Italian Trulli"></i>
-    <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-    <a href="#" class="w3-bar-item w3-button w3-right w3-padding-large">Contact</a>
-    <a href="#band" class="w3-bar-item w3-button w3-right w3-padding-large w3-hide-small">About US</a>
-    <a href="#tour" class="w3-bar-item w3-button w3-right w3-padding-large w3-hide-small">Lettings</a>
-    <a href="#contact" class="w3-bar-item  w3-button w3-right w3-padding-large w3-hide-small">Sales</a>
-    <a href="#contact" class="w3-bar-item w3-button w3-right w3-padding-large w3-hide-small">Property Managment</a>
-    <a href="#contact" class="w3-bar-item  w3-button w3-right w3-padding-large w3-hide-small">Home</a>
+<!-- CSS -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
     
+    <link href="http://localhost/k2_project/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://localhost/k2_project/assets/css/w3.css" rel="stylesheet">
+    <link href="http://localhost/k2_project/assets/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma">
+    <!-- JS -->
+    <script src="http://localhost/k2_project/assets/js/popper.min.js"></script>
+    <script src="http://localhost/k2_project/assets/js/bootstrap.min.js"></script>
+    <script src="http://localhost/k2_project/assets/js/jquery.min.js"></script>
+    <style>
+    div,h1,h2,h3,h4,h5,h6,p{font-family: "Karma", sans-serif}
+    </style>
+  </head>
+  <body>
     
+    <!-- Navbar (sit on top) -->
+    <div class="w3-top">
+      <div class="w3-bar  w3-card w3-text-white" style="background-color: rgb(64, 47, 35);"><p style="margin-left: 50px; letter-spacing: 3px;font-family: 'Times New Roman'; font-size: 20px">Email INFO@ENNISOMORE ESTATE  |  Call: +44(0)20 3123 5677</p>
+    </div>
+
+
+    <!-- Second Logo -->
+    <div class="w3-bar w3-white  w3-padding">
+      <i><img src="http://localhost/k2_project/assets/image/logo1.png" style="margin-left: 40px;" height="50px" alt="Italian Trulli"></i>
+      <i class="fa fa-instagram fa-2x w3-right w3-hover-opacity"></i>
+      <i class="fa fa-twitter fa-2x w3-right w3-hover-opacity"></i>
+      <i class="fa fa-linkedin fa-2x w3-right w3-hover-opacity"></i>
+    </div>
+
+    
+      <!-- Third Bar -->
+    <div class="w3-bar w3-white" id="myNavbar">
+      <i><img src="http://localhost/k2_project/assets/image/logo2.png" height="40px" style="margin-left: 40px;" alt="Italian Trulli"></i>
+      <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="toggleFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+      <a href="http://localhost/k2_project/html/web/contactus.php" class="w3-bar-item w3-button w3-right w3-hide-medium w3-hide-small w3-padding-large">Contact</a>
+      <a href="#band" class="w3-bar-item w3-button w3-right w3-padding-large w3-hide-medium w3-hide-small">About US</a>
+      <a href="http://localhost/k2_project/html/web/lettings.php" class="w3-bar-item w3-button w3-right w3-padding-large w3-hide-small">Lettings</a>
+      <a href="http://localhost/k2_project/html/web/sales.php" class="w3-bar-item  w3-button w3-right w3-padding-large w3-hide-small">Sales</a>
+      <a href="http://localhost/k2_project/html/web/property_managment.php" class="w3-bar-item w3-button w3-right w3-padding-large w3-hide-small">Property Managment</a>
+      <a href="http://localhost/k2_project/index.php"  class="w3-bar-item  w3-button w3-right w3-padding-large w3-hide-small">Home</a>
+      </div>
+    <!-- Navbar on small screens -->
+    <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium">
+      <a href="http://localhost/k2_project/index.php"  class="w3-bar-item w3-button" onclick="toggleFunction()">Home</a>
+      <a href="http://localhost/k2_project/html/web/sales.php" class="w3-bar-item w3-button" onclick="toggleFunction()">Sales</a>
+      <a href="http://localhost/k2_project/index.php"  class="w3-bar-item w3-button" onclick="toggleFunction()">About Us</a>
+      <a href="http://localhost/k2_project/html/web/contactus.php"  class="w3-bar-item w3-button" onclick="toggleFunction()">Contact</a>
+      <a href="http://localhost/k2_project/html/web/lettings.php" class="w3-bar-item w3-button" onclick="toggleFunction()">Lettings</a>
+      <a href="http://localhost/k2_project/html/web/property_managment.php" class="w3-bar-item w3-button" onclick="toggleFunction()">Property Managment</a>
+      
+    </div>
   </div>
-</div>
-
-<!-- Navbar on small screens (remove the onclick attribute if you want the navbar to always show on top of the content when clicking on the links) -->
-<div id="navDemo" class="w3-bar-block w3-black w3-hide w3-hide-large w3-hide-medium w3-top" style="margin-top:46px">
-  <a href="#band" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">BAND</a>
-  <a href="#tour" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">TOUR</a>
-  <a href="#contact" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">CONTACT</a>
-  <a href="#" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">MERCH</a>
-</div>
-
