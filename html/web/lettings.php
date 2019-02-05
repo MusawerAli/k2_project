501e6bfb3a64034013c8b4a28841a8abc9f87217
MusawerAli <pakjalihouse@gmail.com>
2019-02-04 23:40:41 -0800

    lettings.php(complete)

---
+++ html/web/lettings.php
@@ -0,0 +1,101 @@
+<?php
+///include "http://localhost/k2_project/html/header_footer/header.php";
+require_once('../header_footer/header.php');
+?>
+<!-- Header -->
+<header class="w3-display-container w3-content w3-wide" style="max-width:1600px;min-width:500px;margin-top:200px;"  width="1600" height="600" id="home">
+  <img class="w3-image" src="../../assets/image/Website-design-14.png"  alt="Hamburger Catering">
+  <div class="w3-display-middle w3-center w3-text-black w3-white w3-grayscale-max w3-opacity-min">
+    <h1>Rentings a Property</h1>
+    <p class="w3-hide">Just Lets</p>
+    
+    <p class="w3-large w3-text-black w3-hide-small w3-text-white ">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit,</p>
+    <button class="w3-btn w3-black">More Info >></button>
+  </div>
+</header>
+<hr>
+<div class="w3-row-padding">
+  
+  <div class="w3-col m3">
+    <label><i class="fa fa-calendar-o"></i> Area</label>
+    <input class="w3-input w3-border" type="text" placeholder="Any">
+  </div>
+  <div class="w3-col m3">
+    <label><i class="fa fa-male"></i> Maximum Bedrooms</label>
+    <input class="w3-input w3-border" type="number" placeholder="Any">
+  </div>
+  <div class="w3-col m3">
+    <label><i class="fa fa-child"></i> Maximum Price</label>
+    <input class="w3-input w3-border" type="number" placeholder="Any">
+  </div>
+  <div class="w3-col m3">
+    <label><i class="fa fa-search"></i> Search</label>
+    <button class="w3-button w3-block w3-black">Search</button>
+  </div>
+</div>
+<hr>
+<!-- About Section -->
+<!-- !PAGE CONTENT! -->
+<div class="w3-main w3-content w3-padding" style="max-width:1200px;margin-top:100px">
+  <!-- First Photo Grid-->
+  <div class="w3-row-padding w3-padding-16 w3-center"  id="food">
+    <div class="w3-third " style="background-color: rgb(133, 123, 109);">
+      <img src="../../assets/image/Website-design-20.png" alt="Sandwich" style="width:100%">
+      <h3>CHELSEA SQUARE SW3</h3>
+      <p class="w3-wide">£15,000 PER WEEK </p>
+      <b class="w3-bold">3 BEDS | 3 BATHS | 2 RECEPTIONS
+      </b>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet dui finibus, bibendum erat at, tempus diam. Vestibulum quis lorem ac lacus bibendum consectetur. In ac nunc interdum, molestie ante eget, lacinia magna. Nullam suscipit, ligula at sagittis maximus, mauris augue sagittis tellus, sit amet viverra augue lacus id nunc. Ut cursus odio sed metus egestas, </p>
+    </div>
+    
+    <div class="w3-third"  style="background-color: rgb(133, 133, 109);">
+      <img src="../../assets/image/Website-design-20.png" alt="Cherries" style="width:100%">
+      <h3>CHELSEA SQUARE SW3</h3>
+      <p class="w3-wide">£15,000 PER WEEK </p>
+      <b class="w3-bold">3 BEDS | 3 BATHS | 2 RECEPTIONS
+      </b>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet dui finibus, bibendum erat at, tempus diam. Vestibulum quis lorem ac lacus bibendum consectetur. In ac nunc interdum, molestie ante eget, lacinia magna. Nullam suscipit, ligula at sagittis maximus, mauris augue sagittis tellus, sit amet viverra augue lacus id nunc. Ut cursus odio sed metus egestas, </p>
+    </div>
+    <div class="w3-third"  style="background-color: rgb(133,123, 109);">
+      <img src="../../assets/image/Website-design-20.png" alt="Pasta and Wine" style="width:100%">
+      <h3>CHELSEA SQUARE SW3</h3>
+      <p class="w3-wide">£15,000 PER WEEK </p>
+      <b class="w3-bold">3 BEDS | 3 BATHS | 2 RECEPTIONS
+      </b>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet dui finibus, bibendum erat at, tempus diam. Vestibulum quis lorem ac lacus bibendum consectetur. In ac nunc interdum, molestie ante eget, lacinia magna. Nullam suscipit, ligula at sagittis maximus, mauris augue sagittis tellus, sit amet viverra augue lacus id nunc. Ut cursus odio sed metus egestas, </p>
+    </div>
+  </div>
+  <hr>
+  <!-- Second Photo Grid-->
+  <div class="w3-row-padding w3-padding-16 w3-center" style="background-color: rgb(133, 116, 102);" id="food">
+    <div class="w3-third">
+      <img src="../../assets/image/Website-design-20.png" alt="Sandwich" style="width:100%">
+      <h3>CHELSEA SQUARE SW3</h3>
+      <p class="w3-wide">£15,000 PER WEEK </p>
+      <b class="w3-bold">3 BEDS | 3 BATHS | 2 RECEPTIONS
+      </b>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet dui finibus, bibendum erat at, tempus diam. Vestibulum quis lorem ac lacus bibendum consectetur. In ac nunc interdum, molestie ante eget, lacinia magna. Nullam suscipit, ligula at sagittis maximus, mauris augue sagittis tellus, sit amet viverra augue lacus id nunc. Ut cursus odio sed metus egestas, </p>
+    </div>
+    
+    <div class="w3-third"  style="background-color: rgb(133, 100, 119);">
+      <img src="../../assets/image/Website-design-20.png" alt="Cherries" style="width:100%">
+      <h3>CHELSEA SQUARE SW3</h3>
+      <p class="w3-wide">£15,000 PER WEEK </p>
+      <b class="w3-bold">3 BEDS | 3 BATHS | 2 RECEPTIONS
+      </b>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet dui finibus, bibendum erat at, tempus diam. Vestibulum quis lorem ac lacus bibendum consectetur. In ac nunc interdum, molestie ante eget, lacinia magna. Nullam suscipit, ligula at sagittis maximus, mauris augue sagittis tellus, sit amet viverra augue lacus id nunc. Ut cursus odio sed metus egestas, </p>
+    </div>
+    <div class="w3-third"  style="background-color: rgb(133, 123, 109);">
+      <img src="../../assets/image/Website-design-20.png" alt="Pasta and Wine" style="width:100%">
+      <h3>CHELSEA SQUARE SW3</h3>
+      <p class="w3-wide">£15,000 PER WEEK </p>
+      <b class="w3-bold">3 BEDS | 3 BATHS | 2 RECEPTIONS
+      </b>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet dui finibus, bibendum erat at, tempus diam. Vestibulum quis lorem ac lacus bibendum consectetur. In ac nunc interdum, molestie ante eget, lacinia magna. Nullam suscipit, ligula at sagittis maximus, mauris augue sagittis tellus, sit amet viverra augue lacus id nunc. Ut cursus odio sed metus egestas, </p>
+    </div>
+  </div>
+</div>
+<?php
+//include "http://localhost/k2_project/html/header_footer/header.php";
+require_once('../header_footer/footer.php');
+?>
